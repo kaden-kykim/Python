1eca0c1c5caa81953a96e920797c6a2ee3234a80
kaden-kykim <kaden.kykim@gmail.com>
2020-04-27 12:27:57 -0700

    Release version 1.3.0   - Change 'level' argument to the 3rd   - Add the 2nd argument for applying indentation (default value: False)

--- HeadFirst/Chap02_Sharing_Your_Code/nester/setup.py
+++ HeadFirst/Chap02_Sharing_Your_Code/nester/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nester-kaden',
-    version='1.2.0',
+    version='1.3.0',
     py_modules=['nester'],
     author='kaden-kykim',
     author_email='kaden.kykim@gmail.com',
